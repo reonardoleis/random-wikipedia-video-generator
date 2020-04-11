5d42d064340654cd1ea5cee387923aa57dbe9a02
Leonardo Reis da Silva <33813822+reonardoleis@users.noreply.github.com>
2020-04-11 11:16:04 -0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,6 +8,6 @@ upload.start_browser()
 old_uploader_url = 'https://www.youtube.com/upload?redirect_to_creator=true&fr=4&ar=1586612008057&nv=1'
 
 for x in range (0, 3):
-    filename = generate.generate('pt')
-    upload.upload(filename, old_uploader_url, "Abrir")
+    filename = generate.generate('en')
+    upload.upload(filename, old_uploader_url, "Open")
     time.sleep(60)
